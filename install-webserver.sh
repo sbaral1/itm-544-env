d9fcd48582e3981897816b027905d9875119d2e1
sbaral1 <sbaral1@hawk..it.edu>
2015-10-27 16:13:39 -0500

    added line for installing php5 and install composer

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install php5 git php5-cur
+curl -s5 htps://getcomposer.org/installer | php
+php composer-phar require aws/aws-sdk-php
 sudo apt-get update -y
 sudo apt-get install -y apache2
 
