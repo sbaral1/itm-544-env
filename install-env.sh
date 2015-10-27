1ca655f54b5204b020ae5a1ce72b23627677a5ca
sbaral1 <sbaral1@hawk..it.edu>
2015-10-27 15:50:19 -0500

    modified

--- install-env.sh
+++ install-env.sh
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-d05e75b8 --count 2 --instance-type t2.micro --key-name satyajit --security-group-ids sg-877562e0 --subnet-id subnet-5a391703 --associate-public-ip-address --user-data file://install-webserver.sh --debug
+aws ec2 run-instances --image-id ami-d05e75b8 --count 3 --instance-type t2.micro --key-name satyajit --security-group-ids sg-877562e0 --subnet-id subnet-5a391703 --associate-public-ip-address --user-data file://install-webserver.sh --wait --iam-instance-profile sbaral1--debug
