640f61076a3486c33db403f64ee9d9601395995d
Ubuntu <ubuntu@ip-10-0-1-238.ec2.internal>
2015-11-08 21:42:11 +0000

    Updated to move php files

--- install-webserver.sh
+++ install-webserver.sh
@@ -5,6 +5,18 @@ curl -s5 htps://getcomposer.org/installer | php
 php composer-phar require aws/aws-sdk-php
 sudo apt-get update -y
 sudo apt-get install -y apache2
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
 
+git clone https://github.com/sbaral1/itm-544-env
+
+mv ./itm-544-env/images /var/www/html/images
+mv ./itm-544-env/index.html /var/www/html
+mv ./itm-544-env/*.php /var/www/html
+
+curl -sS https://getcomposer.org/installer | php
+
+php composer.phar require aws/aws-sdk-php
+
+mv vendor /var/www/html
 
 echo "Hello World!" > /tmp/hello.txt
